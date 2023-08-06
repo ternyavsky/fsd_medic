75dcd333f0911b6fe6659367eed816e139711632
ternyavsky <ilya.ternyavsky@yandex.ru>
2023-08-06 20:41:00 +0300

    memcache

--- fsd_medic/settings.py
+++ fsd_medic/settings.py
@@ -118,16 +118,12 @@ DATABASES = {
 #}
 
 
-
 CACHES = {
-    'default': {
-        'BACKEND': 'django_redis.cache.RedisCache',
-        'LOCATION': 'redis://127.0.0.1:6379/',
-        'OPTIONS': {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-        }
+    "default": {
+        "BACKEND": "django.core.cache.backends.memcached.PyMemcacheCache",
+        "LOCATION": "127.0.0.1:11211",
     }
-        }
+}
 
 
 
