9e6c6603c120a382113bec8029c513e74e4adc3c
Makar Goncharov <makar6002@mail.ru>
2023-08-27 20:23:02 +0500

    Interview

---
+++ auth_doctor/serializers.py
@@ -0,0 +1,30 @@
+from rest_framework import serializers
+
+from api.models import Interview, User
+from social.models import Chat
+
+from rest_framework import status, exceptions
+from django.contrib.auth import authenticate
+from django.utils.translation import gettext_lazy as _
+
+from loguru import logger
+logger.add("logs/auth_doctor.log", format="{time} {level} {message}", level="DEBUG", rotation="12:00", compression="zip")
+
+
+class InterviewSerializer(serializers.Model):
+    '''Упрвление собесами'''
+    number = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    group = serializers.CharField()
+
+    def create(self, validated_data):
+        return Interview(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.number = validated_data.get('number', instance.number)
+        instance.first_name = validated_data.get('first_name', instance.first_name)
+        instance.last_name = validated_data.get('last_name', instance.last_name)
+        instance.group = validated_data.get('group', instance.group)
+
+        return instance
--- auth_doctor/views.py
+++ auth_doctor/views.py
@@ -1,3 +1,41 @@
-from django.shortcuts import render
 
-# Create your views here.
+import logging
+
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+from api.serializers import CenterSerializer, UserGetSerializer
+from auth_doctor.serializers import InterviewSerializer
+from auth_user.serializers import *
+from auth_user.service import generate_verification_code, send_sms
+from db.queries import *
+
+logger = logging.getLogger(__name__) 
+
+
+class DoctorView(generics.ListCreateAPIView):
+    permission_classes = [AllowAny]
+    """Работа с сотрудниками"""
+    serializer_class = UserGetSerializer
+    queryset = get_users()
+
+    def post(self, request):
+        serializer = InterviewSerializer(data=request.data, context={'request': request})
+        if serializer.is_valid():
+            interview = serializer.save()
+            # interview.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+class CenterRegistrationView(APIView):
+    permission_classes = [AllowAny]
+
+    def get(self, request, city):
+        centers = get_centers().filter(city=city)
+        logger.debug(centers)
+        data = CenterSerializer(centers, many=True).data
+        logger.debug(data)
+        logger.debug(request.path) 
+        return Response(data, status=status.HTTP_200_OK)
