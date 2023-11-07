752e17efb47deb957ef359ec305e439abeb21fab
ternyavsky <ilya.ternyavsky@yandex.ru>
2023-11-07 18:37:51 +0300

    notify

--- social/urls.py
+++ social/urls.py
@@ -5,5 +5,5 @@ from .views import ChatView, MessageView, NotifyView
 urlpatterns = [
     path('api/chat/<int:user_id>/', ChatView.as_view(), name='chat_view_url'),  # User chats with user_id
     path('api/messages/<int:chat_id>/', MessageView.as_view(), name='messages_view_url'),  # Chat messages with chat_id
-    path('api/notifications/<int:user_id>', NotifyView.as_view(), name='notify_view_url')
+    path('api/notifications/', NotifyView.as_view(), name='notify_view_url')
 ]
