2a3f372dd2dd2cb93fae6f4d4fc695ffd8299e7a
ternyavsky <ilya.ternyavsky@yandex.ru>
2023-11-24 10:36:04 +0300

    docker

--- fsd_medic/settings.py
+++ fsd_medic/settings.py
@@ -202,7 +202,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("localhost", 6379)],
+            "hosts": [("172.17.0.1", 6379)],
         },
     },
 }
