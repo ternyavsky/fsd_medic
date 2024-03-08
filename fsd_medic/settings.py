a85880553627435b9f37675abf6ca43d9bf5571a
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-03-08 13:53:08 +0300

    asd

--- fsd_medic/settings.py
+++ fsd_medic/settings.py
@@ -7,6 +7,7 @@ AUTH_USER_MODEL = 'api.User'
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+
 load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
