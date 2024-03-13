552270b66d0c1e43dbac343ea717c12da8a4c319
ternyavsky <ilya.ternyavsky@yandex.ru>
2024-03-13 19:15:27 +0300

    pre

--- fsd_medic/settings.py
+++ fsd_medic/settings.py
@@ -8,6 +8,7 @@ AUTH_USER_MODEL = "api.User"
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+com = 1
 
 load_dotenv(os.path.join(BASE_DIR, ".env"))
 
